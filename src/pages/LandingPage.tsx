7f94c2d4173fb24ccf2e3e12c80dba107f630f37
Diego <99410932+DrGomez21@users.noreply.github.com>
2025-01-20 21:59:03 -0300

    mejoras en estilos visuales

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -13,11 +13,11 @@ import deviceFrame from "../assets/android-device.png";
 export const LandingPage = () => {
   return (
     <>
-      <h1 className="text-4xl text-[#000000] font-bold text-center py-6">Hovy</h1>
+      <h1 className="text-4xl text-[#000000] font-yeseva text-center py-6">Hovy</h1>
 
       <div className="flex flex-col mx-4 text-center space-y-4">
-        <h2 className="text-[#000000] text-2xl font-bold">El creador de paletas de colores <u>más rápido</u></h2>
-        <span className="text-[#000000] text-lg">Obtén los mejores colores para tu marca o sitio web.</span>
+        <h2 className="text-[#000000] text-2xl font-yeseva">El creador de paletas de colores <u>más rápido</u></h2>
+        <span className="text-[#000000] text-lg font-poppins">Obtén los mejores colores para tu marca o sitio web.</span>
       </div>
 
       <div className="flex justify-center mx-4 mt-8">
@@ -28,13 +28,13 @@ export const LandingPage = () => {
       </div>
 
       <div className="flex flex-col space-y-4 mx-4 mt-6">
-        <button className="bg-[#99FFCC] border-2 border-black text-[#000000] shadow-[.3rem_.3rem_#121212] w-full font-semibold text-xl px-4 py-2 rounded-lg hover:scale-95 hover:shadow-none transition duration-100">Empezar a crear</button>
-        <button className="bg-white border-2 border-black text-black shadow-[.3rem_.3rem_#121212] w-full font-semibold text-xl px-4 py-2 rounded-lg hover:scale-95 hover:shadow-none transition duration-100">Código en Github</button>
+        <button className="bg-[#99FFCC] border-2 border-black text-[#000000] shadow-[.3rem_.3rem_#121212] w-full font-yeseva text-xl px-4 py-2 rounded-lg hover:scale-95 hover:shadow-none transition duration-100">Empezar a crear</button>
+        <button className="bg-white border-2 border-black text-black shadow-[.3rem_.3rem_#121212] w-full font-yeseva text-xl px-4 py-2 rounded-lg hover:scale-95 hover:shadow-none transition duration-100">Código en Github</button>
       </div>
 
       <div className="flex flex-col space-y-4 mx-4 mt-6">
 
-        <span className="text-black text-lg font-normal text-center">Colores ideales para UI, diseño gráfico y más.</span>
+        <span className="text-black text-lg font-poppins text-center">Colores ideales para UI, diseño gráfico y más.</span>
 
         <div className="flex gap-5 justify-between mx-4">
           <div className="h-52 flex flex-col w-[130px] border-2 border-[#000000] rounded-lg shadow-[.3rem_.3rem_#121212] overflow-hidden">
@@ -50,8 +50,13 @@ export const LandingPage = () => {
 
       </div>
 
-      <footer className="flex justify-center mx-4 mt-8">
-        <span className="text-sm">Desarrollado con cariño ❤️ @Godie.codes</span>
+      <footer className="flex flex-col items-center mt-8 pb-4">
+        <div className="flex items-center justify-between gap-4">
+          <span className="text-4xl text-[#000000] font-yeseva text-center py-6">Hovy</span>
+          <span className="text-sm font-poppins">Desarrollado con cariño ❤️</span>
+        </div>
+
+        <a href="https://www.instagram.com/godie.codes/" className="font-poppins no-underline text-black">@Godie.codes</a>
       </footer>
     </>
   )
